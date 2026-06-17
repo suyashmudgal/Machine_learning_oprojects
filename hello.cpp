3867db7dd6125bb25686737811359db3125368fc
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-06-17 13:54:36 +0530

    code

--- hello.cpp
+++ hello.cpp
@@ -1,20 +1,29 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int sum;
-    cin>>sum;
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]+arr[j]==sum){
-                cout<<arr[i]<<" "<<arr[j]<<endl;
-            }
+int main()
+{
+    int arr[] = {5,7,10,12,15};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int target = 22;
+    int i=0,j=n-1;
+    while(i<j)
+    {
+        if(arr[i]+arr[j]==target)
+        {
+            cout<<arr[i]<<" "<<arr[j]<<endl;
+            i++;
+            j--;
+        }
+        else if(arr[i]+arr[j]>target)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
         }
     }
+    return 0;
 }
+
