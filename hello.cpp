b8d91fb13f3010bff757462d7392a890dde4ae91
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-06-18 21:25:43 +0530

    code

--- hello.cpp
+++ hello.cpp
@@ -1,16 +1,22 @@
-class Solution {
-public:
-    double angleClock(int hour, int minutes) {
-        // 1 hour = 30 degrees, 1 minute khisakne par hour hand 0.5 degree chalta hai
-        double hourAngle = (hour % 12) * 30.0 + minutes * 0.5;
-        
-        // 1 minute = 6 degrees
-        double minuteAngle = minutes * 6.0;
-        
-        // Absolute difference
-        double angle = abs(hourAngle - minuteAngle);
-        
-        // Smaller angle chahiye
-        return min(angle, 360.0 - angle);
+#include <vector>
+using namespace std;
+
+// Helper function to handle recursion
+void generateNumbers(int current, int x, vector<int> &ans) {
+    // Base case: stop when current number exceeds x
+    if (current > x) {
+        return;
     }
-};
+    
+    // Add the current number to the vector
+    ans.push_back(current);
+    
+    // Recursive call for the next number
+    generateNumbers(current + 1, x, ans);
+}
+
+vector<int> printNos(int x) {
+    vector<int> ans;
+    generateNumbers(1, x, ans);
+    return ans;
+}
