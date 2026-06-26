76b21b45ec7afcdc20dba7fd9da2ec4f0762a466
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-06-26 23:48:06 +0530

    logistic regression in c

--- hello.c
+++ hello.c
@@ -22,12 +22,15 @@ public:
             
             max_ending_here = max(arr[i], max_ending_here + arr[i]);
             
+            // with deletion update:
+            // option 1: deletion already use karke current add karo
+            // option 2: current element ko delete karo (toh sum = prev_max_ending_here)
             max_ending_here_with_deletion = max(
                 max_ending_here_with_deletion + arr[i],
                 prev_max_ending_here
             );
             
-           
+            // answer update
             ans = max(ans, max(max_ending_here, max_ending_here_with_deletion));
         }
         
