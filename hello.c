d89a5cadc4449bc4adfb938418f3bd55d38e3014
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-06-27 23:43:28 +0530

    logistic regression in c

--- hello.c
+++ hello.c
@@ -1,82 +1,55 @@
-#include "lib.h"
-#include "utils.h"
 
-void list_directory(const char *path)
-{
-    DIR *dir = opendir(path);
-    if (!dir)
-    {
-        perror("Failed to open directory");
-        return;
-    }
+#include <stdio.h>
 
-    struct dirent *entry;
-    struct stat file_stat;
-    int index = 1;
+#define alpha 0.01
+#define beta 0.9
+#define epoch 100
 
-    printf("Directory: %s\n", path);
-    printf("---------------------------------------\n");
-
-    while ((entry = readdir(dir)) != NULL)
-    {
-        if (strcmp(entry->d_name, ".") == 0)
-            continue;
-
-        char fullpath[PATH_MAX];
-        snprintf(fullpath, sizeof(fullpath), "%s/%s", path, entry->d_name);
-
-        if (stat(fullpath, &file_stat) == -1)
-        {
-            perror("stat error");
-            continue;
-        }
-        printf("%2d.%-25s", index++, entry->d_name);
-
-        if (S_ISDIR(file_stat.st_mode))
-        {
-            printf("    <DIR>\n");
-        }
-        else
-        {
-            printf("    %ld bytes\n", file_stat.st_size);
-        }
-    }
-    printf("%2d. .. (Go Back)\n", index);
-    printf("-------------------------------------------\n");
-    closedir(dir);
+double gradient(double x){
+  return 2*x;
 }
 
-void view_file(const char *filepath)
+void func(double initValue_x, double initValue_v)
 {
-    FILE *fp = fopen(filepath, "r");
-    if (!fp)
-    {
-        perror("Failed to open file");
-        return;
-    }
+  printf("\n#############################################################################\n");
+  printf("#    oldValue(x)    oldValue(v)     gradient   newValue(v)    newValue(x)   #\n");
+  printf("#############################################################################\n");
 
-    char line[1024];
-    printf("\n--- File: %s ---\n", filepath);
-    while (fgets(line, sizeof(line), fp) != NULL)
-    {
-        printf("%s", line);
-    }
-    printf("\n--- End of File ---\n");
+  double oldValue_x  = initValue_x;
+  double oldValue_v = initValue_v;
+  double gradient_val = gradient(oldValue_x);
+  
+  double newValue_v = beta * oldValue_v + (1 - beta) * gradient_val;
+  double newValue_x = oldValue_x - alpha * newValue_v;
+  
+  
+  for(int i = 1; i <= epoch; i++)
+  {
+    printf("#      %lf       %lf      %lf      %lf      %lf    #\n", oldValue_x, oldValue_v, gradient_val, newValue_v, newValue_x);
 
-    fclose(fp);
+    oldValue_x  = newValue_x;
+    oldValue_v = newValue_v;
+    gradient_val = gradient(oldValue_x);
+
+    newValue_v = beta * oldValue_v + (1 - beta) * gradient_val;
+    newValue_x = oldValue_x - alpha * newValue_v;
+  }
+  printf("\n#############################################################################\n");
 }
 
-int is_directory(const char *path)
+int main()
 {
-    struct stat statbuf;
-    if (stat(path, &statbuf) != 0)
-        return 0;
-    return S_ISDIR(statbuf.st_mode);
-}
+  double value_x;
+  double value_v;
 
-char *join_path(const char *base, const char *entry)
-{
-    static char fullpath[PATH_MAX];
-    snprintf(fullpath, sizeof(fullpath), "%s/%s", base, entry);
-    return fullpath;
+  printf("Enter Initial value of x: ");
+  scanf("%lf", &value_x);
+
+  printf("Enter Initial value of v: ");
+  scanf("%lf", &value_v);
+
+  printf("Gradient Descent with Momentum of function x^2 with initial values of x = %lf and v = %lf", value_x, value_v);
+  func(value_x, value_v);
+  
+  return 0;
 }
