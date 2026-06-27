b10e03d838240f6ff088af55004f6f881bea3148
Suyash Mudgal <suyashmudgal18@gmail.com>
2026-06-27 23:45:03 +0530

    logistic regression in c

--- hello.c
+++ hello.c
@@ -1,55 +1,42 @@
 
 #include <stdio.h>
 
-#define alpha 0.01
-#define beta 0.9
-#define epoch 100
+#define lr 0.01
+#define MAX_ITER 100
+#define FUNC x*x
 
 double gradient(double x){
   return 2*x;
 }
 
-void func(double initValue_x, double initValue_v)
+void GD(double initValue)
 {
-  printf("\n#############################################################################\n");
-  printf("#    oldValue(x)    oldValue(v)     gradient   newValue(v)    newValue(x)   #\n");
-  printf("#############################################################################\n");
+  printf("\n##################################################\n");
+  printf("#    OldValue  #    Gradient    #     NewValue   #\n");
+  printf("##################################################\n");
 
-  double oldValue_x  = initValue_x;
-  double oldValue_v = initValue_v;
-  double gradient_val = gradient(oldValue_x);
-  
-  double newValue_v = beta * oldValue_v + (1 - beta) * gradient_val;
-  double newValue_x = oldValue_x - alpha * newValue_v;
-  
-  
-  for(int i = 1; i <= epoch; i++)
+  double oldValue = initValue;
+  double gradient_val = gradient(oldValue);
+  double newValue = oldValue - lr * gradient_val;
+
+  for(int i = 0; i < MAX_ITER; i++)
   {
-    printf("#      %lf       %lf      %lf      %lf      %lf    #\n", oldValue_x, oldValue_v, gradient_val, newValue_v, newValue_x);
-
-    oldValue_x  = newValue_x;
-    oldValue_v = newValue_v;
-    gradient_val = gradient(oldValue_x);
-
-    newValue_v = beta * oldValue_v + (1 - beta) * gradient_val;
-    newValue_x = oldValue_x - alpha * newValue_v;
+    printf("#    %lf  #    %lf    #    %lf    #\n", oldValue, gradient_val, newValue);
+    
+    oldValue = newValue;
+    gradient_val  = gradient(newValue);
+    newValue = oldValue - lr * gradient_val;
   }
-  printf("\n#############################################################################\n");
+  printf("##################################################\n");
 }
-
 int main()
 {
-  double value_x;
-  double value_v;
-
-  printf("Enter Initial value of x: ");
-  scanf("%lf", &value_x);
-
-  printf("Enter Initial value of v: ");
-  scanf("%lf", &value_v);
-
-  printf("Gradient Descent with Momentum of function x^2 with initial values of x = %lf and v = %lf", value_x, value_v);
-  func(value_x, value_v);
+  double value;
+  printf("Enter a value: ");
+  scanf("%lf", &value);
   
+  printf("Gradient Descent on the function x^2 with starting value %lf\n", value);
+  GD(value);
+
   return 0;
 }
